a98b6b3a86fb42e9093518655272409338ffba37
Anke <36001381+dxhw99@users.noreply.github.com>
2022-10-16 15:52:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@ sed -i "/helloworld/d" "feeds.conf.default"
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
+echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> "feeds.conf.default"
