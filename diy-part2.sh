6a3db5806cc7ad05e3d7fec4a7d26b60c665a4bc
Anke <36001381+dxhw99@users.noreply.github.com>
2024-05-19 21:24:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,8 +13,8 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 #设置默认IP为 10.0.0.2
-#sed -i 's/192.168.1.1/10.0.0.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.63/g' package/base-files/files/bin/config_generate
 # 设置默认IP为 Ip段 10.0.
 #sed -i 's/192.168./10.0./g' package/base-files/files/bin/config_generate
 # 设置默认主机名 Openiot
-#sed -i 's/'OpenWrt'/'Openiot'/g' package/base-files/files/bin/config_generate
+sed -i 's/'OpenWrt'/'znziot'/g' package/base-files/files/bin/config_generate
