51db79a1825dd4004ace5eff6e02a6079d240ac8
Anke <36001381+dxhw99@users.noreply.github.com>
2024-07-27 19:35:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,9 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-#设置默认IP为 10.0.0.2
+#设置默认IP为 10.0.0.63
 sed -i 's/192.168.1.1/10.0.0.63/g' package/base-files/files/bin/config_generate
-# 设置默认IP为 Ip段 10.0.
-#sed -i 's/192.168./10.0./g' package/base-files/files/bin/config_generate
-# 设置默认主机名 Openiot
+# 设置默认主机名 znziot
 sed -i 's/'OpenWrt'/'znziot'/g' package/base-files/files/bin/config_generate
 # 加入编译者信息
 sed -i "s/OpenWrt /Anke build $(TZ=UTC-8 date "+%Y.%m.%d") @OpenWrt /g" ../lede/package/lean/default-settings/files/zzz-default-settings
