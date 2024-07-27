2cf15d880c1bbbfb52540bbee8911d3169c272dc
Anke <36001381+dxhw99@users.noreply.github.com>
2024-07-27 21:10:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -16,4 +16,4 @@ sed -i 's/192.168.1.1/10.0.0.63/g' package/base-files/files/bin/config_generate
 # 设置默认主机名 znziot
 sed -i 's/'OpenWrt'/'znziot'/g' package/base-files/files/bin/config_generate
 # 加入编译者信息
-sed -i "s/OpenWrt /Anke build $(TZ=UTC-8 date "+%Y.%m.%d") @OpenWrt /g" ../lede/package/lean/default-settings/files/zzz-default-settings
+#sed -i "s/OpenWrt /Anke build $(TZ=UTC-8 date "+%Y.%m.%d") @OpenWrt /g" ../lede/package/lean/default-settings/files/zzz-default-settings
